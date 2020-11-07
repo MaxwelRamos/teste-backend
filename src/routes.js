30e19521874669e5aacab4f5fdc769f832b5722f
MaxwelRamos <gronesoft.atendimento@gmail.com>
2020-11-07 16:28:57 -0300

    criacao rota login

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,52 @@
+// require('dotenv').config();
+
 const routes = require('express').Router();
 const User = require('./models/User');
 const MoedasEnviadas = require('./models/MoedasEnviadas');
 const Yup = require('yup');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const configAuth = require('../auth');
+
+
 
 routes.get("/", (req,res) => {
-    return res.json({ Local: 'Raiz2'});
+    return res.json({ Local: 'Raiz3'});
 });
 
 //Routes relacionadas ao Usuario
+routes.post("/login", async (req,res) => {
+    const { email, password } = req.body;
+
+    const usuario = await User.findOne({email: email});
+    if (!usuario){
+        return res.status(401).json({
+            error: true,
+            code: 106,
+            message: "Erro: Usuário ou Senha não confere!"
+        })
+    }
+
+    if (!(await bcrypt.compare(password, usuario.password))){
+        return res.status(401).json({
+            error: true,
+            code: 106,
+            message: "Erro: Usuário/Senha não confere!"
+        })
+    }
+
+    const token = jwt.sign({id: usuario._id}, configAuth.secret, {expiresIn: configAuth.expiresIn})
+
+    return res.json({
+        error: false,
+        user: {
+            id: usuario.id,
+            email
+        },
+        token
+    });
+});
+
 routes.get("/users", async (req,res) => {
     await User.find({}).select("-password").then((users) => {
         return res.json({
